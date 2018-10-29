860c7e4eb8ba3f950c7461e5d95652fca72abe12
Vladimir Tsikhon <uladzimir_tsikhon@epam.com>
2018-10-29 15:30:45 +0300

    Update Calculator.cs
    Add Subtr implementation

--- SimpleCalc/SimpleCalc/Calculator.cs
+++ SimpleCalc/SimpleCalc/Calculator.cs
@@ -39,7 +39,8 @@ namespace SimpleCalc
 		//Create a method to subtract the two numbers
 		public double Subtract()
 		{
-			throw new NotImplementedException();
+			//throw new NotImplementedException();
+			return Number1 - Number2;
 		}
 
 		//Create a method to multiply the two numbers
