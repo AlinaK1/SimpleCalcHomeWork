5deb8bcd25ec9b11b846b50981225d223b8289b9
Vladimir Tsikhon <uladzimir_tsikhon@epam.com>
2018-11-20 17:06:12 +0300

    Update Calculator.cs

--- SimpleCalc/SimpleCalc/Calculator.cs
+++ SimpleCalc/SimpleCalc/Calculator.cs
@@ -46,7 +46,7 @@ namespace SimpleCalc
 		//Create a method to multiply the two numbers
 		public double Multiply()
 		{
-			throw new NotImplementedException();
+		   return Number1* Number2;
 		}
 
 		//Create a method to divide the two numbers
